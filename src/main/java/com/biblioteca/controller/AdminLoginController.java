243ced4d2241e3a60b9f5d4c7ec10b7e78bb5f8f
camilo <estudio5654@gmail.com>
2024-06-02 10:56:10 -0500

    Actualizacion

--- src/main/java/com/biblioteca/controller/AdminLoginController.java
+++ src/main/java/com/biblioteca/controller/AdminLoginController.java
@@ -18,18 +18,23 @@ public class AdminLoginController {
 
     @GetMapping("/admin/login")
     public String loginForm() {
-        return "/login";
+        return "admin/login"; // Cambia "/login" a "admin/login"
     }
 
     @PostMapping("/admin/login")
     public String login(@RequestParam String username, @RequestParam String password, Model model) {
-        Administrador admin = administradorRepository.findByUsername(username);
-        if (admin != null && admin.getPassword().equals(password)) {
-            // Autenticación exitosa, redirigir a la página de inicio del administrador
-            return "redirect:/admin/list";
-        } else {
-            model.addAttribute("error", "Usuario o contraseña incorrectos");
-            return "/login";
+        try {
+            Administrador admin = administradorRepository.findByUsername(username);
+            if (admin != null && admin.getPassword().equals(password)) {
+                // Autenticación exitosa, redirigir a la página de inicio del administrador
+                return "redirect:/admin/list";
+            } else {
+                model.addAttribute("error", "Usuario o contraseña incorrectos");
+                return "admin/login"; // Cambia "/login" a "admin/login"
+            }
+        } catch (Exception e) {
+            model.addAttribute("error", "Se ha producido un error inesperado");
+            return "admin/login"; // Cambia "/login" a "admin/login"
         }
     }
 
